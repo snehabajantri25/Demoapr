f341b30efad74e0f2986b3d052aac7a0a541a848
Snehabajantri25 <Snehabajantri051@gmail.com>
2025-04-23 16:47:03 +0530

    save

--- demo69.java
+++ demo69.java
@@ -2,13 +2,18 @@ public class demo69 {
     public static void main(String[] args) {
         int num1 = 10; // First number
         int num2 = 5;  // Second number
-
+       
         int add = num1 + num2;
         int sub = num1 - num2;
+        int mul = num1 + num2;
+        int div = num1 - num2;
        
        
         System.out.println("Addition Result: " +add);
         System.out.println("Subtraction Result: " + sub);
+        System.out.println("Multiplication Result: " +mul);
+        System.out.println("Division Result: " + div);
+        
         
     }
 }
