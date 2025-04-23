127d7448cb1527596ff7e8932b5155a5b8c2493f
Snehabajantri25 <Snehabajantri@25>
2025-04-23 15:05:22 +0530

    save

---
+++ addsub.java
@@ -0,0 +1,12 @@
+public class addsub {
+    public static void main(String[] args) {
+        int num1 = 10; // First number
+        int num2 = 5;  // Second number
+
+        int mul = num1 + num2;
+        int div = num1 - num2;
+
+        System.out.println("Addition Result: " +mul);
+        System.out.println("Subtraction Result: " + div);
+    }
+}
