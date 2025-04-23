32f64bc274d3295621fdf9885e98a564e06f0314
Snehabajantri25 <Snehabajantri@25>
2025-04-23 15:19:58 +0530

    svae

---
+++ demo69.java
@@ -0,0 +1,12 @@
+public class demo69 {
+    public static void main(String[] args) {
+        int num1 = 10; // First number
+        int num2 = 5;  // Second number
+
+        int add = num1 + num2;
+        int sub = num1 - num2;
+
+        System.out.println("Addition Result: " +add);
+        System.out.println("Subtraction Result: " + sub);
+    }
+}
